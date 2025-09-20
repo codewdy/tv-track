a1f24afa89743ed1fbf7c1ccac8dbaf37e53e062
codewdy <codewdy@gmail.com>
2025-09-20 17:34:33 +0800

    add channel searcher

--- server/searcher/web_searcher/search_schema.py
+++ server/searcher/web_searcher/search_schema.py
@@ -5,3 +5,11 @@ class Subject(BaseModel):
     name: str
     url: str
     cover_url: str
+
+
+class Channel(BaseModel):
+    class Episode(BaseModel):
+        name: str
+        url: str
+    name: str
+    episodes: list["Channel.Episode"]
