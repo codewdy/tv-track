11e1eaa4814a60a2631463812606de914944c952
codewdy <codewdy@gmail.com>
2025-09-21 12:32:12 +0800

    remove auto for debugging

--- server/main.py
+++ server/main.py
@@ -4,7 +4,8 @@ from service.web_handler import web_routes
 import os
 
 auth = BasicAuthMiddleware(username='user', password='password')
-app = web.Application(middlewares=[auth])
+# app = web.Application(middlewares=[auth])
+app = web.Application()
 app.add_routes(web_routes(
     '/', os.path.join(os.path.dirname(__file__), '../web/dist'), 'index.html'))
 web.run_app(app, port=8080)
