f6f6b632c26be689c1ebc60baa1a4a732bfd8abb
codewdy <codewdy@gmail.com>
2025-10-02 15:33:29 +0800

    filter name

--- web/src/components/AddTV/TVName.vue
+++ web/src/components/AddTV/TVName.vue
@@ -1,14 +1,17 @@
 <template>
     <n-space vertical>
-        <n-input v-model:value="name" placeholder="请输入名字" />
+        <n-input v-model:value="name" placeholder="请输入名字" :status="error ? 'error' : undefined" />
         <n-text type="error" v-if="error">{{ error }}</n-text>
     </n-space>
 </template>
 
 <script setup lang="ts">
 import { NInput, NText, NSpace } from 'naive-ui'
-import { computed, ref, defineModel, watch } from 'vue'
+import { computed, ref, defineModel, watch, inject } from 'vue'
+import type { monitor } from '@/schema'
+import type { Ref } from 'vue'
 
+const tvs = inject('tvs') as Ref<monitor.TV[]>
 const name = ref<string>('')
 
 const error = computed(() => {
@@ -33,8 +36,8 @@ function validateName(name: string): string {
         return '请输入名字'
     }
     // TODO: Check name not in db
-    if (name === "123") {
-        return '名字不能为123'
+    if (tvs.value.some((tv) => tv.name === name)) {
+        return '名字已存在'
     }
     return ''
 }
