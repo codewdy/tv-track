6d76423960b14ad9e70ab064a549b837a6b19e3f
codewdy <codewdy@gmail.com>
2025-10-07 11:22:21 +0800

    add all update

--- web/src/components/common/UpdateSource.vue
+++ web/src/components/common/UpdateSource.vue
@@ -1,12 +1,23 @@
 <template>
-    <SearchSource v-model:result="source" />
-    <n-button @click="updateEpisodeSource" :disabled="source == null">修改一集</n-button>
+    <n-space vertical>
+        <SearchSource v-model:result="source" />
+        <n-button @click="updateEpisodeSource" :disabled="source == null">修改一集</n-button>
+        <n-space>
+            <n-checkbox v-model:checked="tracking" size="large">
+                跟踪更新
+            </n-checkbox>
+            <n-checkbox v-model:checked="update_downloaded" size="large">
+                重新下载
+            </n-checkbox>
+            <n-button @click="updateSource" :disabled="source == null">修改源</n-button>
+        </n-space>
+    </n-space>
 </template>
 
 <script setup lang="ts">
 import { ref } from 'vue'
 import axios from 'axios'
-import { NButton, useMessage } from 'naive-ui'
+import { NSpace, NButton, NCheckbox, useMessage } from 'naive-ui'
 import SearchSource from './SearchSource.vue';
 import type { db } from '@/schema';
 
@@ -19,6 +30,8 @@ const { tv_id, episode_idx } = defineProps<{
 const emit = defineEmits(["done"])
 
 const source = ref<db.Source | null>(null)
+const tracking = ref<boolean>(false)
+const update_downloaded = ref<boolean>(false)
 
 async function updateEpisodeSource() {
     if (source.value == null) {
@@ -28,6 +41,23 @@ async function updateEpisodeSource() {
         id: tv_id,
         episode_idx: episode_idx,
         source: source.value.episodes[episode_idx]
+    }).catch(() => {
+        message.error('修改失败')
+    }).then(() => {
+        message.success('修改成功')
+        emit("done")
+    })
+}
+
+async function updateSource() {
+    if (source.value == null) {
+        return
+    }
+    source.value.tracking = tracking.value
+    await axios.post('/api/update_source', {
+        id: tv_id,
+        update_downloaded: update_downloaded.value,
+        source: source.value
     }).catch(() => {
         message.error('修改失败')
     }).then(() => {
