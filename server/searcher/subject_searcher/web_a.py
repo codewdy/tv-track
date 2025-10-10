02fb67315a5d7f16427a56b731672c9511a6ab6c
codewdy <codewdy@gmail.com>
2025-10-11 06:39:51 +0800

    fix girigirilove

--- server/searcher/subject_searcher/web_a.py
+++ server/searcher/subject_searcher/web_a.py
@@ -7,13 +7,20 @@ import urllib
 class WebASubjectSearcher(WebSubjectSearcher):
     def __init__(self, config):
         super().__init__(config)
+        self.token = config["token"]
         self.a = config["a"]
         self.cover = config["cover"]
         self.cover_attr = config["cover_attr"]
 
     def parse(self, src, soup):
-        tokens = soup.select(self.a)
+        tokens = soup.select(self.token)
+        a_s = soup.select(self.a)
         covers = soup.select(self.cover)
+
+        if len(tokens) != len(a_s):
+            raise RuntimeError(
+                f"cannot parse search result len(tokens)={len(tokens)} len(a_s)={len(a_s)}"
+            )
 
         if len(tokens) != len(covers):
             raise RuntimeError(
@@ -23,7 +30,7 @@ class WebASubjectSearcher(WebSubjectSearcher):
         return [
             Subject(
                 name=to_text(token),
-                url=urllib.parse.urljoin(src, token["href"]),
+                url=urllib.parse.urljoin(src, a["href"]),
                 cover_url=urllib.parse.urljoin(src, cover[self.cover_attr]))
-            for token, cover in zip(tokens, covers)
+            for token, a, cover in zip(tokens, a_s, covers)
         ]
